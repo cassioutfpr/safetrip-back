9697ab3a4b0f4a91da57f29d585e2ac6434ca70e
Cassio Morales <cassioutfpr@gmail.com>
2021-09-29 20:29:15 -0300

    getLon

--- src/main/java/safetrip/Main.java
+++ src/main/java/safetrip/Main.java
@@ -166,7 +166,7 @@ public class Main {
         for (int i = 0; i < latitudes; i++ ) {
             System.out.println(String.valueOf(round(res.getBest().getPoints().getLat(i), 5)) + "," + 
                     String.valueOf(round(res.getBest().getPoints().getLon(i),5)) + "|");
-            //lineString += String.valueOf(round(res.getBest().getPoints().getLng(i), 5)) + " " + String.valueOf(round(res.getBest().getPoints().getLat(i),5)) + ", ";
+            lineString += String.valueOf(round(res.getBest().getPoints().getLng(i), 5)) + " " + String.valueOf(round(res.getBest().getPoints().getLat(i),5)) + ", ";
         }
         
         lineString = lineString.substring(0, lineString.length() - 2) + ")";
