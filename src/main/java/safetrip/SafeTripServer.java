61bc8f1fabb578b0ab12e05fc758fc9a7d485d6d
Luiz Agner <luizagnern@hotmail.com>
2021-10-05 00:25:10 -0300

    Introducing HTTPS Server

--- src/main/java/safetrip/SafeTripServer.java
+++ src/main/java/safetrip/SafeTripServer.java
@@ -1,14 +1,14 @@
 package safetrip;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.*;
 import org.apache.commons.io.FileUtils;
 
+import javax.net.ssl.*;
 import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.URL;
+import java.security.KeyStore;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Map;
@@ -28,6 +28,7 @@ public class SafeTripServer {
         System.out.println("Arkivo abaxado");
         
         startHttpServer();
+        startHttpsServer();
              
         //DatabaseConnectionManager.queryPreparedStatement(
         //        "SELECT * FROM public.acidentes, public.trechos WHERE ST_INTERSECTS(trechos.geom, ST_GeomFromText(?, 4326));",
@@ -37,11 +38,66 @@ public class SafeTripServer {
     
     
     public static void startHttpServer() throws IOException {
-        int port = Integer.parseInt(System.getenv("PORT"));
+        int port = Integer.parseInt(System.getenv("HTTP_PORT"));
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
         server.createContext("/route", new AccessResourceIntent());
         server.setExecutor(null); // creates a default executor
         server.start();
+    }
+
+    public static void startHttpsServer() {
+        try {
+            // setup the socket address
+            InetSocketAddress address = new InetSocketAddress(Integer.parseInt(System.getenv("HTTPS_PORT")));
+
+            // initialise the HTTPS server
+            HttpsServer httpsServer = HttpsServer.create(address, 0);
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+
+            // initialise the keystore
+            char[] password = System.getenv("CERTIFICATE_PASSWORD").toCharArray();
+            KeyStore ks = KeyStore.getInstance("JKS");
+            FileInputStream fis = new FileInputStream("testkey.jks");
+            ks.load(fis, password);
+
+            // setup the key manager factory
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+            kmf.init(ks, password);
+
+            // setup the trust manager factory
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+            tmf.init(ks);
+
+            // setup the HTTPS context and parameters
+            sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+            httpsServer.setHttpsConfigurator(new HttpsConfigurator(sslContext) {
+                public void configure(HttpsParameters params) {
+                    try {
+                        // initialise the SSL context
+                        SSLContext context = getSSLContext();
+                        SSLEngine engine = context.createSSLEngine();
+                        params.setNeedClientAuth(false);
+                        params.setCipherSuites(engine.getEnabledCipherSuites());
+                        params.setProtocols(engine.getEnabledProtocols());
+
+                        // Set the SSL parameters
+                        SSLParameters sslParameters = context.getSupportedSSLParameters();
+                        params.setSSLParameters(sslParameters);
+
+                    } catch (Exception ex) {
+                        System.out.println("Failed to create HTTPS port");
+                    }
+                }
+            });
+            httpsServer.createContext("/route", new AccessResourceIntent());
+            httpsServer.setExecutor(null); // creates a default executor
+            httpsServer.start();
+
+        } catch (Exception exception) {
+            System.out.println("Failed to create HTTPS server on port " + 443 + " of localhost");
+            exception.printStackTrace();
+
+        }
     }
     
     static class AccessResourceIntent implements HttpHandler {        
